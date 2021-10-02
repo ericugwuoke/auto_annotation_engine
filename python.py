c0bf0e71cb95aa5e521ccc1fd96ab4a1a6767737
Eric Ajuruchukwu Ugwuoke <68876488+ericugwuoke@users.noreply.github.com>
2021-10-02 21:27:50 +0100

    removed the need to call sql script
    modified the schema and dropped the need for multiple tables

--- python.py
+++ python.py
@@ -3,18 +3,8 @@ import os
 from datetime import date
 today = date.today()
 date_metadata = today.strftime("%d-%m-%Y")
-sql_sensor_ID="INSERT INTO Sensors (Sensor_ID) VALUES ('_sensorid_');"
-sql_sensor_value="INSERT INTO Sensor_value (Sensor_value) VALUES (_sensorvalue_);"
-sql_longitude="INSERT INTO LONGITUDE  (longitude ) VALUES (_long_);"
-sql_latitude="INSERT INTO Latitude  (latitude ) VALUES (_lat_);"
-sql_observation_domain="INSERT INTO obSERVATION_DOMAIN   (obserVATION_DOMAIN) VALUES ('_domain_');"
-sql_unit="INSERT INTO Unit   (unit ) VALUES ('_unit_');"
-sql_accuracy="INSERT INTO accuracy   (accuracy ) VALUES (_accuracy_);"
-sql_date="INSERT INTO Date   (Date) VALUES ('_date_');"
-sql_platform="INSERT INTO platform    (platform) VALUES ('_platform_');"
-sql_metadata="INSERT INTO metadata    (metadata) VALUES ('_metadata_');"
-sql_time="INSERT INTO time    (time) VALUES ('_time_');"
-database_update_command="java -cp ./h2/bin/h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2:tcp://localhost/~/test -script update_script.sql -user sa"
+#sql_ontolgy="INSERT INTO ontology (SENSORS, SENSOR_VALUE, UNIT, DATE, TIME, OBSERVATION_DOMAIN, METADATA, PLATFORM, LATITUDE, LONGITUDE) VALUES ('_sensorid_',_sensorvalue_,'_unit_', '_date_', _time_, '_domain_', '_metadata_', '_platform_', _lat_, _long_);"
+database_update_command="java -cp /path/to/h2/h2/bin/h2-1.4.200.jar org.h2.tools.Shell -url jdbc:h2:tcp://localhost/~/test -user sa -sql \"INSERT INTO ontology (SENSORS, SENSOR_VALUE, UNIT, ACCURACY, DATE, TIME, OBSERVATION_DOMAIN, METADATA, PLATFORM, LATITUDE, LONGITUDE) VALUES ('_sensorid_', _sensorvalue_, '_unit_', _accuracy_, '_date_', _time_, '_domain_', '_metadata_', '_platform_', _lat_, _long_);\""
 tagX_output=""
 
 while tagX_output !="done":
@@ -27,26 +17,18 @@ while tagX_output !="done":
             sensor_metadata=tagX_output.split(' ')[1].split('_')
             #create sql file
             if len(sensor_metadata) == 9:
-                update_script= open("update_script.sql", "w")
-                update_script.write(sql_sensor_ID.replace("_sensorid_",sensor_metadata[0])+"\n")
-                update_script.write(sql_sensor_value.replace("_sensorvalue_",sensor_output)+"\n")
-                update_script.write(sql_longitude.replace("_long_",sensor_metadata[2])+"\n")
-                update_script.write(sql_latitude.replace("_lat_",sensor_metadata[1])+"\n")
-                update_script.write(sql_observation_domain.replace("_domain_",sensor_metadata[5])+"\n")
-                update_script.write(sql_unit.replace("_unit_",sensor_metadata[6])+"\n")
-                update_script.write(sql_accuracy.replace("_accuracy_",sensor_metadata[4])+"\n")
-                update_script.write(sql_date.replace("_date_",date_metadata)+"\n")
-                update_script.write(sql_platform.replace("_platform_",sensor_metadata[8])+"\n")
-                update_script.write(sql_metadata.replace("_metadata_",sensor_metadata[7])+"\n")
-                update_script.write(sql_time.replace("_time_",sensor_metadata[3])+"\n")
-                update_script.close()
+                #update_script= open("update_script.sql", "w")
+                #update_script.write(sql_ontolgy.replace("_sensorid_",sensor_metadata[0]).replace("_sensorvalue_",sensor_output).replace("_long_",sensor_metadata[2]).replace("_lat_",sensor_metadata[1]).replace("_domain_",sensor_metadata[5]).replace("_unit_",sensor_metadata[6]).replace("_accuracy_",sensor_metadata[4]).replace("_date_",date_metadata).replace("_platform_",sensor_metadata[8]).replace("_metadata_",sensor_metadata[7]).replace("_time_",sensor_metadata[3]))
+                #update_script.close()
+                sqlupdate=database_update_command.replace("_sensorid_",sensor_metadata[0]).replace("_sensorvalue_",sensor_output).replace("_long_",sensor_metadata[2]).replace("_lat_",sensor_metadata[1]).replace("_domain_",sensor_metadata[5]).replace("_unit_",sensor_metadata[6]).replace("_accuracy_",sensor_metadata[4]).replace("_date_",date_metadata).replace("_platform_",sensor_metadata[8]).replace("_metadata_",sensor_metadata[7]).replace("_time_",sensor_metadata[3])
 
                 print('updating the H2 dtabase with the following scrip \n \n')
                 
-                update_script= open("update_script.sql", "r")
-                print(update_script.read())
+                #update_script= open("update_script.sql", "r")
+                #print(update_script.read())
                 #update h2 database
-                os.system(database_update_command)
+                print(sqlupdate)
+                os.system(sqlupdate)
             else:
                 print ("please try again with correct metatdata format \n")
         except:
